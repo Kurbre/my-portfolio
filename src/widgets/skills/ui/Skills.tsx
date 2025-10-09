b1fde5cd17cbd079b1e9662d9a9cbd6e4e60852a
Kurbre <catiklou@gmail.com>
2025-10-09 11:54:28 +0000

    Completed widget skills.

---
+++ src/widgets/skills/ui/Skills.tsx
@@ -0,0 +1,27 @@
+import { Container } from '../../../shared/ui/container'
+import { Subtitle } from '../../../shared/ui/subtitle'
+import { Title } from '../../../shared/ui/title'
+import { skillsData } from '../data/data'
+import SkillsItem from './SkillsItem'
+
+const Skills = () => {
+	return (
+		<section>
+			<Container>
+				<div className='flex items-center flex-col'>
+					<Subtitle position='center' className='-left-3'>
+						Навыки
+					</Subtitle>
+					<Title className='mt-1 text-center'>Что я использую в работе</Title>
+				</div>
+				<div className='flex gap-5 flex-wrap justify-center mt-10'>
+					{skillsData.map(({ icon, title }) => (
+						<SkillsItem icon={icon} title={title} key={icon + title} />
+					))}
+				</div>
+			</Container>
+		</section>
+	)
+}
+
+export default Skills

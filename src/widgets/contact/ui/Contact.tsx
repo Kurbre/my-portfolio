1284ad4abfe1e49bdc48609373bb50703257de6a
Kurbre <catiklou@gmail.com>
2026-07-28 13:30:28 +0000

    Update exprience info

--- src/widgets/contact/ui/Contact.tsx
+++ src/widgets/contact/ui/Contact.tsx
@@ -1,70 +1,70 @@
-import {Container} from '../../../shared/ui/container'
-import {Subtitle} from '../../../shared/ui/subtitle'
-import {Title} from '../../../shared/ui/title'
-import {useScroll} from '../../../shared/scroll'
+import { Container } from '../../../shared/ui/container'
+import { Subtitle } from '../../../shared/ui/subtitle'
+import { Title } from '../../../shared/ui/title'
+import { useScroll } from '../../../shared/scroll'
 import Form from './Form'
 import photo from '../assets/images/photo.png'
-import {Social} from '../../../shared/ui/social'
-import {ScrollFadeInCenter} from '../../../shared/animation'
+import { Social } from '../../../shared/ui/social'
+import { ScrollFadeInCenter } from '../../../shared/animation'
 
 export const Contact = () => {
-    const {refs} = useScroll()
+	const { refs } = useScroll()
 
-    return (
-        <section className='dark:bg-gray-700' ref={refs.contact}>
-            <Container className='flex gap-8 lg:gap-16 flex-wrap md:flex-nowrap'>
-                <ScrollFadeInCenter initialPosition='left' initialDistance={350}>
-                    <img
-                        src={photo}
-                        alt='Photo section about'
-                        className='w-full h-[600px] lg:w-[400px] lg:h-[600px] object-cover rounded-2xl
+	return (
+		<section className='dark:bg-gray-700' ref={refs.contact}>
+			<Container className='flex gap-8 lg:gap-16 flex-wrap md:flex-nowrap'>
+				<ScrollFadeInCenter initialPosition='left' initialDistance={350}>
+					<img
+						src={photo}
+						alt='Photo section about'
+						className='w-full h-[600px] lg:w-[400px] lg:h-[600px] object-cover rounded-2xl
 						sm:mx-0 mx-auto
 						shadow-lg max-w-full'
-                        draggable={false}
-                        loading='lazy'
-                    />
-                </ScrollFadeInCenter>
-                <div className='w-full'>
-                    <ScrollFadeInCenter initialPosition='right' initialDistance={350}>
-                        <Subtitle>Контакти</Subtitle>
-                    </ScrollFadeInCenter>
-                    <ScrollFadeInCenter
-                        initialPosition='right'
-                        initialDistance={450}
-                        delay={0.5}
-                    >
-                        <Title position='left' className='mt-3'>
-                            Зв’яжіться зі мною
-                        </Title>
-                    </ScrollFadeInCenter>
-                    <ScrollFadeInCenter
-                        initialPosition='right'
-                        initialDistance={550}
-                        delay={1}
-                    >
-                        <p className='text-md font-semibold mt-4'>
-                            Будь-яким зручним для вас способом:
-                        </p>
-                        <Social size={30}/>
-                    </ScrollFadeInCenter>
-                    <ScrollFadeInCenter
-                        initialPosition='right'
-                        initialDistance={650}
-                        delay={1.5}
-                    >
-                        <p className='text-md font-semibold mt-4'>
-                            Або залиште свої дані, і я сама вам напишу:
-                        </p>
-                    </ScrollFadeInCenter>
-                    <ScrollFadeInCenter
-                        initialPosition='right'
-                        initialDistance={350}
-                        delay={1.5}
-                    >
-                        <Form/>
-                    </ScrollFadeInCenter>
-                </div>
-            </Container>
-        </section>
-    )
+						draggable={false}
+						loading='lazy'
+					/>
+				</ScrollFadeInCenter>
+				<div className='w-full'>
+					<ScrollFadeInCenter initialPosition='right' initialDistance={350}>
+						<Subtitle>Контакти</Subtitle>
+					</ScrollFadeInCenter>
+					<ScrollFadeInCenter
+						initialPosition='right'
+						initialDistance={450}
+						delay={0.5}
+					>
+						<Title position='left' className='mt-3'>
+							Зв’яжіться зі мною
+						</Title>
+					</ScrollFadeInCenter>
+					<ScrollFadeInCenter
+						initialPosition='right'
+						initialDistance={550}
+						delay={1}
+					>
+						<p className='text-md font-semibold mt-4'>
+							Будь-яким зручним для вас способом:
+						</p>
+						<Social size={30} />
+					</ScrollFadeInCenter>
+					<ScrollFadeInCenter
+						initialPosition='right'
+						initialDistance={650}
+						delay={1.5}
+					>
+						<p className='text-md font-semibold mt-4'>
+							Або залиште свої дані, і я сам вам напишу:
+						</p>
+					</ScrollFadeInCenter>
+					<ScrollFadeInCenter
+						initialPosition='right'
+						initialDistance={350}
+						delay={1.5}
+					>
+						<Form />
+					</ScrollFadeInCenter>
+				</div>
+			</Container>
+		</section>
+	)
 }
--- src/widgets/experience/ui/Experience.tsx
+++ src/widgets/experience/ui/Experience.tsx
@@ -1,64 +1,70 @@
-import {Container} from '../../../shared/ui/container'
-import {Subtitle} from '../../../shared/ui/subtitle'
-import {Title} from '../../../shared/ui/title'
+import { Container } from '../../../shared/ui/container'
+import { Subtitle } from '../../../shared/ui/subtitle'
+import { Title } from '../../../shared/ui/title'
 import ExperienceColumn from './ExperienceColumn'
 import ExperienceItem from './ExperienceItem'
-import {PiStudent} from 'react-icons/pi'
-import {useScroll} from '../../../shared/scroll'
-import {ScrollFadeInCenter} from '../../../shared/animation'
-import {HiMiniComputerDesktop} from 'react-icons/hi2'
+import { PiStudent } from 'react-icons/pi'
+import { useScroll } from '../../../shared/scroll'
+import { ScrollFadeInCenter } from '../../../shared/animation'
+import { HiMiniComputerDesktop } from 'react-icons/hi2'
 
 const Experience = () => {
-    const {refs} = useScroll()
+	const { refs } = useScroll()
 
-    return (
-        <section className='dark:bg-gray-700' ref={refs.experience}>
-            <Container>
-                <div className='flex items-center flex-col'>
-                    <ScrollFadeInCenter initialPosition='top' initialDistance={150}>
-                        <Subtitle position='center'>Досвід</Subtitle>
-                    </ScrollFadeInCenter>
-                    <ScrollFadeInCenter initialPosition='left' initialDistance={350}>
-                        <Title className='text-center'>Досвід роботи та освіта</Title>
-                    </ScrollFadeInCenter>
-                </div>
-                <div className='flex mt-15 justify-between flex-wrap gap-10'>
-                    <ScrollFadeInCenter initialPosition='left' initialDistance={250}>
-                        <ExperienceColumn>
-                            <ExperienceItem
-                                title='Студент "ЗЕФК"'
-                                subtitle='Розробка програмного забезпечення'
-                                description='Я навчаюся на факультеті розробки програмного забезпечення, вивчаю сучасні технології та методології розробки, щоб створювати якісні та ефективні веб-додатки. Постійно розвиваю навички програмування та працюю над практичними проєктами.'
-                                icon={<PiStudent size={28}/>}
-                            />
-                            <ExperienceItem
-                                title='IT - навички'
-                                subtitle='Самоосвіта'
-                                description='Я активно займаюся самоосвітою в IT: вивчаю нові технології, фреймворки та інструменти, стежу за трендами веб-розробки та реалізую власні проєкти, щоб покращувати навички та практичний досвід.'
-                                icon={<HiMiniComputerDesktop size={28}/>}
-                            />
-                        </ExperienceColumn>
-                    </ScrollFadeInCenter>
-                    <ScrollFadeInCenter initialPosition='right' initialDistance={250}>
-                        <ExperienceColumn>
-                            <ExperienceItem
-                                title='Freecourses'
-                                subtitle='7 місяців'
-                                description='Я працював у компанії FreeCourses, де брав участь у розробці та підтримці веб-додатків, створював інтерфейси та інтегрував бекенд через API. Отримав досвід командної роботи, взаємодії із замовниками та дотримання строків проєктів.'
-                                icon={<PiStudent size={28}/>}
-                            />
-                            <ExperienceItem
-                                title='Freelance'
-                                subtitle='1 рік'
-                                description='Я рік працював на фрілансі, виконував замовлення зі створення та підтримки веб-додатків, спілкувався з клієнтами та самостійно керував проєктами. Цей досвід допоміг розвинути відповідальність, вміння планувати задачі та писати якісний код.'
-                                icon={<PiStudent size={28}/>}
-                            />
-                        </ExperienceColumn>
-                    </ScrollFadeInCenter>
-                </div>
-            </Container>
-        </section>
-    )
+	return (
+		<section className='dark:bg-gray-700' ref={refs.experience}>
+			<Container>
+				<div className='flex items-center flex-col'>
+					<ScrollFadeInCenter initialPosition='top' initialDistance={150}>
+						<Subtitle position='center'>Досвід</Subtitle>
+					</ScrollFadeInCenter>
+					<ScrollFadeInCenter initialPosition='left' initialDistance={350}>
+						<Title className='text-center'>Досвід роботи та освіта</Title>
+					</ScrollFadeInCenter>
+				</div>
+				<div className='flex mt-15 justify-between flex-wrap gap-10'>
+					<ScrollFadeInCenter initialPosition='left' initialDistance={250}>
+						<ExperienceColumn>
+							<ExperienceItem
+								title='Студент "ЗЕФК"'
+								subtitle='Розробка програмного забезпечення'
+								description='Я навчаюся на факультеті розробки програмного забезпечення, вивчаю сучасні технології та методології розробки, щоб створювати якісні та ефективні веб-додатки. Постійно розвиваю навички програмування та працюю над практичними проєктами.'
+								icon={<PiStudent size={28} />}
+							/>
+							<ExperienceItem
+								title='IT - навички'
+								subtitle='Самоосвіта'
+								description='Я активно займаюся самоосвітою в IT: вивчаю нові технології, фреймворки та інструменти, стежу за трендами веб-розробки та реалізую власні проєкти, щоб покращувати навички та практичний досвід.'
+								icon={<HiMiniComputerDesktop size={28} />}
+							/>
+						</ExperienceColumn>
+					</ScrollFadeInCenter>
+					<ScrollFadeInCenter initialPosition='right' initialDistance={250}>
+						<ExperienceColumn>
+							<ExperienceItem
+								title='Macroglide'
+								subtitle='1 рік та 4 місяці'
+								description='Я працював Full-stack розробником у компанії Macroglide, де розробляв веб-додатки на Next.js та NestJS, будував REST API та налаштовував бази даних через Prisma. Впроваджував архітектуру FSD, оптимізував продуктивність і брав активну участь у командному плануванні та code review.'
+								icon={<PiStudent size={28} />}
+							/>
+							<ExperienceItem
+								title='Freecourses'
+								subtitle='7 місяців'
+								description='Я працював у компанії FreeCourses, де брав участь у розробці та підтримці веб-додатків, створював інтерфейси та інтегрував бекенд через API. Отримав досвід командної роботи, взаємодії із замовниками та дотримання строків проєктів.'
+								icon={<PiStudent size={28} />}
+							/>
+							<ExperienceItem
+								title='Freelance'
+								subtitle='1 рік'
+								description='Я рік працював на фрілансі, виконував замовлення зі створення та підтримки веб-додатків, спілкувався з клієнтами та самостійно керував проєктами. Цей досвід допоміг розвинути відповідальність, вміння планувати задачі та писати якісний код.'
+								icon={<PiStudent size={28} />}
+							/>
+						</ExperienceColumn>
+					</ScrollFadeInCenter>
+				</div>
+			</Container>
+		</section>
+	)
 }
 
 export default Experience
